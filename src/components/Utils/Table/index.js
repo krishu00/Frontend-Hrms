6ca13990e2594dc0089c771e47931c560d1d97dc
Anandita99 <148131294+Anandita99@users.noreply.github.com>
2024-08-07 18:35:16 +0530

    changes in templates , divided it into three parts

--- src/components/Utils/Table/index.js
+++ src/components/Utils/Table/index.js
@@ -1,7 +1,29 @@
-import React from 'react'
 
-export default function Table() {
+import React from 'react';
+import '../../ComponentsCss/utils/Table/Table.css';
+
+const Table = ({ headers, data, renderActions }) => {
   return (
-    <div>Table</div>
-  )
-}
+    <table className="templates-table">
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={row.id || rowIndex}>
+            {Object.values(row).map((cell, cellIndex) => (
+              <td key={cellIndex}>{cell}</td>
+            ))}
+            {renderActions && <td>{renderActions(row)}</td>}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
