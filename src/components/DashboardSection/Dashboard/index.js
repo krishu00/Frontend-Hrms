8fa87a4893cb5af3ff8e7d8342b6e330eecbd485
sakshi <sakshiaggarwal207@gmail.com>
2024-08-12 13:44:03 +0530

    styling

--- src/components/DashboardSection/Dashboard/index.js
+++ src/components/DashboardSection/Dashboard/index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Navbar from "../Navbar";
-import Templates from "../TemplatesComponents/Index";
+// import Navbar from "../Navbar";
+// import Templates from "../TemplatesComponents/Index";
+// import CommonButtons from "../../Utils/CommonButtons";
+ import AssetsForm from "../../Utils/AssetsForm/index.js";
 
 export default function Dashboard() {
   return (
     <div>
-      <Navbar />
-      <Templates/>
+     <AssetsForm/>
+      {/* <CommonButtons />
+      {/* <SecondaryButton/> */}
+      {/* <h3>BUTTON</h3>  */}
     </div>
   );
 }
