4ad4b004ab399c71085064ecebc0934d9bebbff2
Anandita99 <148131294+Anandita99@users.noreply.github.com>
2024-08-12 18:16:36 +0530

    0005/anandita/08-08-24

--- src/components/DashboardSection/Dashboard/index.js
+++ src/components/DashboardSection/Dashboard/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Navbar from "../Navbar";
+//import Navbar from "../Navbar";
 import Templates from "../TemplatesComponents/Index";
+import AssetsForm from "../../Utils/AssetForm";
 
 export default function Dashboard() {
   return (
     <div>
-      <Navbar />
-      <Templates/>
+      <Templates/> 
+      <AssetsForm/>
     </div>
   );
 }
