ed5c45d0dc5f7d1db1688f3ac4743fdc4c7651f1
shivamofficial81 <shivamofficial81@gmail.com>
2024-08-23 18:59:19 +0530

    RequestForm

--- src/components/DashboardSection/Dashboard/index.js
+++ src/components/DashboardSection/Dashboard/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Templates from "../TemplatesComponents/Index";
+import RequestForm from "../../Utils/RequestForm";
 
 export default function Dashboard() {
   return (
     <div>
-      <Navbar />
-      <Templates/>
+  
+      <RequestForm title="Create An Account" cancelBtn={true} />
+      {/* <RequestForm/> */}
+      
     </div>
   );
 }
